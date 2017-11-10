944d305054653c8f50982654c49b319f5304516f
Daniel Rees <daniel.rees@autodata.net>
2017-11-10 09:45:19 -0500

    Have it working with no channel close ... would like to fix that

--- message.go
+++ message.go
@@ -5,12 +5,23 @@ import (
 	"github.com/streadway/amqp"
 )
 
+const queueName string = "etl-dashboard"
+
 type Message struct {
 	Env map[string]string `json:"env"`
 }
 
-type Messenger interface {
+type Sender interface {
 	Send(msg Message, routingKey string, correlationId string) error
+}
+
+type Watcher interface {
+	Watch(routingKey string) (<-chan amqp.Delivery,error)
+}
+
+type Messenger interface {
+	Watcher
+	Sender
 }
 
 type RabbitMessenger struct {
@@ -23,6 +34,7 @@ func NewRabbitMessenger(conn *amqp.Connection, exchangeName string) Messenger {
 }
 
 func (rm RabbitMessenger) Send(msg Message, routingKey string, correlationId string) error {
+	//TODO: Consider here, you create and dispose of a channel each send ... figure out how to keep the publish channel alive
 	ch, err := rm.connection.Channel()
 	if err != nil {
 		return err
@@ -43,4 +55,59 @@ func (rm RabbitMessenger) Send(msg Message, routingKey string, correlationId str
 			Body:          body,
 		},
 	)
+}
+
+func (rm RabbitMessenger) Watch(routingKey string) (<-chan amqp.Delivery,error){
+	ch, err := rm.connection.Channel()
+	if err != nil {
+		return nil,err
+	}
+	//defer ch.Close()
+	err = ch.ExchangeDeclare(
+		rm.exchangeName,
+		"topic",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil,err
+	}
+	q, err := ch.QueueDeclare(
+		queueName,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil,err
+	}
+	err = ch.QueueBind(
+		q.Name,
+		"#",
+		rm.exchangeName,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil,err
+	}
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil,err
+	}
+
+	return msgs,nil
 }
